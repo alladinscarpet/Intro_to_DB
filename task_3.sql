919cc882795a7794d98d567a964b3bfa9e9290e0
alladinscarpet <dmw3ndwa@gmail.com>
2025-10-19 21:51:36 +0300

    Full description

---
+++ task_3.sql
@@ -0,0 +1,5 @@
+-- Write a script that list all the tables in alx_book_store in your MySQL server.
+
+USE alx_book_store;
+
+SHOW TABLES;
---
+++ task_4.sql
@@ -0,0 +1,18 @@
+-- Write a script that prints the full description of the table books
+-- from the database alx_book_store in your MySQL server.
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+
+USE alx_book_store;
+
+-- DESCRIBE Books;
+
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE,
+    IS_NULLABLE,
+    COLUMN_KEY,
+    COLUMN_DEFAULT,
+    EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store'
+  AND TABLE_NAME = 'Books';
