824c96152d2de7e465a9b853f518c24819e5fd90
alladinscarpet <dmw3ndwa@gmail.com>
2025-10-19 22:00:13 +0300

    Populating DB

---
+++ task_5.sql
@@ -0,0 +1,6 @@
+-- Write a script that inserts a single row
+-- in the table customer (database alx_book_store) in your MySQL server
+
+USE alx_book_store;
+
+INSERT INTO Customers VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
