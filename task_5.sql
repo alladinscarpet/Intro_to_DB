5de5c93cb78f989118bc9baf6a9b88d298b63574
alladinscarpet <dmw3ndwa@gmail.com>
2025-10-19 22:09:46 +0300

    Populating DB4

--- task_5.sql
+++ task_5.sql
@@ -5,5 +5,6 @@ USE alx_book_store;
 
 DESCRIBE Customers;
 
+ALTER TABLE Customers RENAME TO customer;
 
-INSERT INTO Customers(customer_id, customer_name, email, address) VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
+INSERT INTO customer(customer_id, customer_name, email, address) VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "123 Happiness Ave.");
